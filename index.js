882b5a4d42fe8603c36b8ad4a5bca95f6a3257b7
devin-lynch <devin.l.lynch@gmail.com>
2023-06-01 23:28:30 -0500

    Test api call

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const app = express();
 
 app.use(cors());
 
+const clientID = process.env.MAL_CLIENT_ID;
+
+const apiCall = async () => {
+    try {
+        const url =
+            'https://api.myanimelist.net/v2/anime/10357?fields=rank,mean,alternative_titles';
+        const response = await fetch(url, {
+            headers: {
+                'X-MAL-CLIENT-ID': clientID,
+            },
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.warn(error);
+    }
+};
+
+apiCall();
+
 app.get('/', (req, res) => {
     res.send('Home page!');
 });
